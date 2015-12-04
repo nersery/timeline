9c169a61e124db1227bdb9c8fe452c5f25ffaab1
voidsatisfaction <lourie@naver.com>
2015-12-04 15:35:05 +0900

    Making timeline text

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'wall/write' => 'wall#write'
+
+  get 'wall/posts' => 'wall#posts'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
