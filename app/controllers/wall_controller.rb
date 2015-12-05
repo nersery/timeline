6f3a8d40553bea86c5e7fbae17ba8eeca79c6c72
voidsatisfaction <lourie@naver.com>
2015-12-05 20:06:33 +0900

    Adding timeline posts save function and redirect function

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -2,7 +2,16 @@ class WallController < ApplicationController
   def write
   end
 
-  def write_complete
+  def write_complete #writer, content
+    p = Post.new
+    p.name = params[:writer]
+    p.content = params[:content]
+    if p.save
+      redirect_to "/wall/posts"
+    else
+      flash[:alert] = p.errors[:content][0]
+      redirect_to :back
+    end
   end
 
   def posts
