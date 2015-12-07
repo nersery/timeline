b80944d5e752b3be1272736aec20163c04f1a2e7
voidsatisfaction <lourie@naver.com>
2015-12-07 09:31:49 +0900

    Making migration and model files and create comment table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+  has_many :comments
   validates :content, :presence => { :message => "please write something."}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205093449) do
+ActiveRecord::Schema.define(version: 20151207002153) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "name"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
