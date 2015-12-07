2d788b5b99d01be5a696064592c7114cf7661f5f
voidsatisfaction <lourie@naver.com>
2015-12-07 15:03:19 +0900

    Adding saving signup function and redirection

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20151207050606) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "passoword"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
