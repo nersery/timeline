f741922e0a7f72205593d4b9b3c896d0bbd6d4d9
voidsatisfaction <lourie@naver.com>
2015-12-07 14:35:06 +0900

    Making migration, model files and creating User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207002153) do
+ActiveRecord::Schema.define(version: 20151207050606) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20151207002153) do
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "passoword"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
